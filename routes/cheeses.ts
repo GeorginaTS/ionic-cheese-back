635c134a90d657e9b6f659b202a050161f59c979
Georgina <georginatomass@gmail.com>
2025-09-30 18:46:03 +0200

    feat: change likeCheese route from POST to PUT for consistency with RESTful practices

--- routes/cheeses.ts
+++ routes/cheeses.ts
@@ -7,7 +7,7 @@ import {getAllCheeses, getOneCheese, createCheese, updateOneCheese, deleteOneChe
 router.get("/", verifyToken, getAllCheeses)
 router.get("/public", getAllPublicCheeses)
 router.get("/public/:id", getOnePublicCheeses)
-router.post("/public/like/:id", verifyToken, likeCheese)
+router.put("/public/like/:id", verifyToken, likeCheese)
 router.get("/:id", verifyToken, getOneCheese)
 router.post("/", verifyToken, createCheese)
 router.put("/:id", verifyToken, updateOneCheese) 
